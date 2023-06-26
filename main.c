4c6b9a23509e40dbba911a9acf68334a087a315b
janasek <janasek.git@proton.me>
2023-06-26 23:09:11 +0200

    creating CMakeList and updating main
    adding some structures to main and some basic error handling

--- main.c
+++ main.c
@@ -1,5 +1,55 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(void){
-    return 0;
+typedef struct person
+{
+    char *name;
+    char *surname;
+ 
+} person;
+
+
+typedef struct persons
+{
+    unsigned int capacity;
+    unsigned int last_idx;
+    person *person_arr;
+
+} persons;
+
+
+typedef struct day
+{
+    uint8_t day;
+    uint8_t month;
+    persons * persons_arr;
+
+} day;
+
+
+typedef struct heap
+{
+    unsigned int capacity;
+    unsigned int last_idx;
+    day *birthdays;
+
+} heap;
+
+
+int main(int argc, char **args)
+{
+    if (argc > 3)
+    {
+        fprintf(stderr, "Too much arguments, try -h (help).");
+        return EXIT_FAILURE;
+    }
+
+    if (argc == 2 && strcmp(args[1], "-h") == 0){
+        printf("-h for help\n-i 'DD-MM NAME SURNAME' for adding new person\n-d 'DD-MM NAME SURNAME' to delete birthday\n-l ['DATE OF BIRTH'] or ['NAME SURNAME'] to list birth day\n-s to print soonest birthday\n");
+        return EXIT_SUCCESS;
+    }
+
+    return EXIT_SUCCESS;
 }
